ee427d4910b02660d708f6b78110e5f6f6f5d5fc
Staffan Olsson <staffan@repos.se>
2017-06-09 10:55:25 +0200

    Basically restarting from stdout plugin, to debug and because we don't need json encoding

--- out_kafka.go
+++ out_kafka.go
@@ -1,24 +1,17 @@
 package main
 
+import "github.com/fluent/fluent-bit-go/output"
 import (
-  "github.com/fluent/fluent-bit-go/output"
   "github.com/ugorji/go/codec"
   "github.com/Shopify/sarama"
-  "encoding/json"
-  "reflect"
-  "unsafe"
   "fmt"
-  "io"
+  "unsafe"
   "C"
+  "reflect"
 )
 
 var brokerList []string = []string{"kafka-0.broker.kafka.svc.cluster.local:9092","kafka-1.broker.kafka.svc.cluster.local:9092","kafka-2.broker.kafka.svc.cluster.local:9092"}
 var producer sarama.SyncProducer
-
-//export FLBPluginRegister
-func FLBPluginRegister(ctx unsafe.Pointer) int {
-  return output.FLBPluginRegister(ctx, "out_kafka", "out_kafka GO!")
-}
 
 //export FLBPluginInit
 func FLBPluginInit(ctx unsafe.Pointer) int {
@@ -30,121 +23,53 @@ func FLBPluginInit(ctx unsafe.Pointer) int {
     return output.FLB_ERROR
   }
 
-  return output.FLB_OK
+  return output.FLBPluginRegister(ctx, "out_kafka", "out_kafka GO!")
 }
 
 //export FLBPluginFlush
 func FLBPluginFlush(data unsafe.Pointer, length C.int, tag *C.char) int {
-  var h codec.MsgpackHandle
-
+  var count int
+  var h codec.Handle = new(codec.MsgpackHandle)
   var b []byte
   var m interface{}
   var err error
-  var enc_data []byte
 
   b = C.GoBytes(data, length)
-  dec := codec.NewDecoderBytes(b, &h)
+  dec := codec.NewDecoderBytes(b, h)
 
   // Iterate the original MessagePack array
+  count = 0
   for {
-    // decode the msgpack data
+    // Decode the entry
     err = dec.Decode(&m)
     if err != nil {
-      if err == io.EOF {
-        break
-      }
-      fmt.Printf("Failed to decode msgpack data: %v\n", err)
-      return output.FLB_ERROR
+      break
     }
 
-    // select format until config files are available for fluentbit
-    format := "json"
+    // Get a slice and their two entries: timestamp and map
+    slice := reflect.ValueOf(m)
+    timestamp := slice.Index(0)
+    data := slice.Index(1)
 
-    if format == "json" {
-      enc_data, err = encode_as_json(m)
-    } else if format == "msgpack" {
-      enc_data, err = encode_as_msgpack(m)
-    } else if format == "string" {
-      // enc_data, err == encode_as_string(m)
+    // Convert slice data to a real map and iterate
+    map_data := data.Interface().(map[interface{}] interface{})
+    fmt.Printf("[%d] %s: [%d, {", count, C.GoString(tag), timestamp)
+    for k, v := range map_data {
+      fmt.Printf("\"%s\": %v, ", k, v)
     }
-    if err != nil {
-      fmt.Printf("Failed to encode %s data: %v\n", format, err)
-      return output.FLB_ERROR
-    }
-
-    producer.SendMessage(&sarama.ProducerMessage {
-      Topic: "logs_default",
-      Key:   nil,
-      Value: sarama.ByteEncoder(enc_data),
-    })
-    
+    fmt.Printf("}\n")
+    count++
   }
+
+  // Return options:
+  //
+  // output.FLB_OK    = data have been processed.
+  // output.FLB_ERROR = unrecoverable error, do not try this again.
+  // output.FLB_RETRY = retry to flush later.
   return output.FLB_OK
 }
 
-  // record is map of interface to interfaces, which the json Marshaler will
-  // not encode automagically. we need to iterate over it, and create a new
-  // map of strings to interfaces that the json Marshaler can handle
-func prepare_data(record interface{}) interface{} {
-  // base case, 
-  // if val is map, return
-  r, ok := record.(map[interface{}] interface{})
-
-  if ok != true {
-    return record
-  }
-
-  record2 := make(map[string] interface{})
-  for k, v := range r {
-    key_string := k.(string)
-    // convert C-style string to go string, else the JSON encoder will attempt
-    // to base64 encode the array
-    if val, ok := v.([]byte); ok {
-      // if it IS a byte array, make string
-      v2 := string(val)
-      // add to new record map
-      record2[key_string] = v2
-    } else {
-      // if not, recurse to decode interface &
-      // add to new record map
-      record2[key_string] = prepare_data(v)
-    }
-  }
-
-  return record2
-}
-
-func encode_as_json(m interface {}) ([]byte, error) {
-  slice := reflect.ValueOf(m)
-  timestamp := slice.Index(0).Interface().(uint64)
-  record := slice.Index(1).Interface()
-
-  type Log struct {
-    Time uint64
-    Record interface{}
-  }
-
-  log := Log {
-    Time: timestamp,
-    Record: prepare_data(record),
-  }
-
-  return json.Marshal(log)
-} 
-
-func encode_as_msgpack(m interface {}) ([]byte, error) {
-  var (
-    mh codec.MsgpackHandle
-    w io.Writer
-    b []byte
-  )
-
-  enc := codec.NewEncoder(w, &mh)
-  enc = codec.NewEncoderBytes(&b, &mh)
-  err := enc.Encode(&m)
-  return b, err
-}
-
+//export FLBPluginExit
 func FLBPluginExit() int {
   return 0
 }
